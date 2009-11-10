185448f3352c77926df3b9e65f638f3efc5bda04
Kevin Roy <kiniou@gmail.com>
2009-11-10 12:34:47 +0100

    Add animation support & move to github :
    - added "fake" animations support to the exported meshes:
        For now it's just copies of the mesh according to animation frames count.
        This enhancement needs matrix support.
    - split git repo and moved to github.

--- nds_3d_export.py
+++ nds_3d_export.py
@@ -1,4 +1,21 @@
 #!BPY
+"""
+    Nintendo DS CallList Exporter for Blender
+    Copyright © 2008, 2009 Kevin Roy
+
+    Nintendo DS CallList Exporter for Blender is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 """ Registration info for Blender menus:
 Name: 'Nintendo DS CallList'
@@ -15,20 +32,7 @@ This script export models in Nintendo DS CallList for
 the DevKitPro SDK in a .h file.
 
 Usage:
-
 Go to Export and type a name for the file.
-
-v0.1:<br>
-    UV-textured meshes + Normals + Colors.
-    Export into "C-style" format.
-    Export Texture into PCX Format with correct size.
-
-v0.2:<br>
-    Export into "Binary" format.
-
-TODO :<br>
-    - 3D Animation support
-    - Export directly into binary format
 """
 
 from Blender.BGL import *
