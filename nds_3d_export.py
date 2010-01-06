7e76a6659fcfc8027e2bde9b6e1d623f01b0702c
Kevin 'kiniou' Roy <kiniou@gmail.com>
2010-01-06 01:03:17 +0100

    Fix a bug with blender menu registering
    * Cleaned GPL license in header to make the script register again in the Export Menu

--- nds_3d_export.py
+++ nds_3d_export.py
@@ -1,21 +1,21 @@
 #!BPY
-"""
-    Nintendo DS CallList Exporter for Blender
-    Copyright © 2008, 2009 Kevin Roy <kiniou_AT_gmail_DOT_com>
 
-    Nintendo DS CallList Exporter for Blender is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+#    Nintendo DS CallList Exporter for Blender
+#    Copyright (C) 2008, 2009 Kevin Roy <kiniou_AT_gmail_DOT_com>
+#
+#    Nintendo DS CallList Exporter for Blender is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
 
 """ Registration info for Blender menus:
 Name: 'Nintendo DS CallList'
